dd48eb27d6080616803908faef5385d6baf84e61
Rafael Minguet <rminguet@gmail.com>
2015-02-03 12:56:02 +0100

    Clases project created

---
+++ clases/modules/core/src/core/models/renderView.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function renderView($request, $config, $data=null)
+{
+//     echo '<pre>';
+//     print_r($config['view_path'].'/'.
+//                 $request['controller'].'/'.
+//                 $request['action'].'.phtml');
+//     echo '</pre>';
+    ob_start();
+        include($config['view_path'].'/'.
+                $request['controller'].'/'.
+                $request['action'].'.phtml'
+               );
+    $content = ob_get_contents();
+    ob_end_clean();
+    return $content;    
+}
